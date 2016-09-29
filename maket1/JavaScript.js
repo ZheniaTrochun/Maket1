282c32b28841abd0ac2ef95c2250f5c43b33fd2e
Zhenia Trochun <zheniatrochun@ukr.net>
2016-09-29 10:06:55 +0300

    v 1.1

---
+++ maket1/JavaScript.js
@@ -0,0 +1,17 @@
+﻿
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
+
+
+$(document).ready(function () {
+    $('ul.tabs').tabs();
+});
+
+$(document).ready(function () {
+    $('.slider').slider({ full_width: true, height: 435, indicators: false });
+});
+
+$(document).ready(function () {
+    $('.slider2').slider({ full_width: true, height: 295, indicators: false });
+});
